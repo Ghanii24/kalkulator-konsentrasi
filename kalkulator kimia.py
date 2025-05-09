7cfd1ba2f910a70414d6b0e04edd1b6bafc5a88b
Ghanii24 <rehanghanii04@gmail.com>
2025-05-09 14:03:58 +0700

    Update kalkulator kimia.py

--- kalkulator kimia.py
+++ kalkulator kimia.py
@@ -6,6 +6,18 @@ st.set_page_config(
     page_icon="⚗️",
     layout="centered"
 )
+st.markdown("""
+    <style>
+    .stApp {
+        background: linear-gradient(rgba(0, 0, 0, 0.85), rgba(0, 0, 0, 0.85)),
+                    url('https://img.freepik.com/free-photo/person-paying-using-nfc-technology_23-2149893740.jpg');
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        color: white !important;
+    }
+    </style>
+    """, unsafe_allow_html=True)
 
 # ==========Judul utama==========
 st.title("⚗️ Kalkulator Konsentrasi Larutan 🧪")
