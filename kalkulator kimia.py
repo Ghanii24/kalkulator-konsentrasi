71b5b915278fe857ef9ad335cb04be0d714c91f8
Ghanii24 <rehanghanii04@gmail.com>
2025-05-10 13:47:18 +0700

    Update kalkulator kimia.py

--- kalkulator kimia.py
+++ kalkulator kimia.py
@@ -10,7 +10,7 @@ st.markdown("""
     <style>
     .stApp {
         background: linear-gradient(rgba(0, 0, 0, 0.85), rgba(0, 0, 0, 0.85)),
-                    url('http://img.freepik.com/free-photo/blue-latex-gloves-holding-plastic-bottle-pouring-transparent-liquid_273609-13624.jpg?t=st=1746776280~exp=1746779880~hmac=8f853b031649b7a5bbce36dee80969e8a7211941c57ed7f27e43cd0aefea1ca6&w=1380');
+                    url('https://img.freepik.com/free-photo/glassware-arrangement-with-red-substance_23-2148794420.jpg?t=st=1746859570~exp=1746863170~hmac=f74d48f4d30b3a74a63715529c650298c8b681ac3d3bb76479e05d6c6f20364a&w=1060');
         background-size: cover;
         background-position: center;
         background-repeat: no-repeat;
